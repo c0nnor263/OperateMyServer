079ae7defdd5ae3bca8363b35d2bf4b7d65d9c98
c0nnor263 <36234732+c0nnor263@users.noreply.github.com>
2026-01-25 14:41:56 +0200

    Initial Release 1.0.0

--- src/main/kotlin/io/conboi/restartmyserver/util/TickTimer.kt
+++
@@ -1,7 +0,0 @@
-package io.conboi.restartmyserver.util
-
-class TickTimer(private val intervalTicks: Int = 20) {
-    fun shouldFire(serverTickCount: Int): Boolean {
-        return serverTickCount % intervalTicks == 0
-    }
-}
